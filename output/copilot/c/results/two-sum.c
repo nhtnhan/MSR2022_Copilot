d451e475bda66a4e7d843e4735744e0bf1b7f30b
Nhan Nguyen <nhnguyen@ualberta.ca>
2022-01-14 13:01:13 -0500

    removed unused files

---
+++ output/copilot/c/results/two-sum.c
@@ -0,0 +1,25 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int *result = malloc(sizeof(int) * 2);
+    *returnSize = 2;
+    int i, j;
+    for (i = 0; i < numsSize; i++) {
+        for (j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+    return result;
+}
---
+++ output/copilot/java/results/uglyNumberII.java
@@ -0,0 +1,20 @@
+/** https://leetcode.com/problems/ugly-number-ii/ */
+
+/**
+ * An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+ * Given an integer n, return the nth ugly number.
+ */
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.min(dp[i2] * 2, Math.min(dp[i3] * 3, dp[i5] * 5));
+            if (dp[i] == dp[i2] * 2) i2++;
+            if (dp[i] == dp[i3] * 3) i3++;
+            if (dp[i] == dp[i5] * 5) i5++;
+        }
+        return dp[n - 1];
+    }
+}
